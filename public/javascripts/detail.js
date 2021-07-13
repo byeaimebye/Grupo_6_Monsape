43af6548b3bfbadc3327a9e0e614fa17374422ff
Aime Agustina Pelozo <aimeagustinagarces@gmail.com>
2021-07-13 12:49:07 -0300

    header html,style modularizado, header css

---
+++ public/javascripts/detail.js
@@ -0,0 +1,15 @@
+const modalAdd = document.querySelector('#modalAdd');
+
+const openModal = () => {
+    modalAdd.style.display = 'flex';
+}
+
+const closeModal = () => {
+    modalAdd.style.display = 'none';
+}
+
+modalAdd.onclick = (event) => {
+    if(event.target == modalAdd){
+        closeModal();
+    }
+}
