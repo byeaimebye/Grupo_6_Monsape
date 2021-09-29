ffa7c0e3e75a69f9ce8e101c1ca085e43fcb2978
aime <aimeagustinagarces@gmail.com>
2021-09-28 21:53:36 -0300

    middleware admin funcionando

--- src/middlewares/adminSession.js
+++ src/middlewares/adminSession.js
@@ -1,6 +1,8 @@
 module.exports = function adminSession(req, res, next){
-    if(req.session.user.rol != "ROL-ADMIN"){
-        res.redirect('/users/login')
+    if(req.session.user && req.session.user.rol === "ROL-ADMIN"){
+        next();
+    }else{
+        res.redirect('/home')
     }
-    next();
+    
 }
