099a3b6e3301349373caf46bd8e8dfff3f32fa63
aime <aimeagustinagarces@gmail.com>
2021-10-10 23:52:22 -0300

    mas js para el header

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,10 +1,15 @@
 window.addEventListener("scroll", () => {
   if(window.scrollY >= 100){
      document.querySelector(".prueba").style.backgroundColor= "rgba(0,0,0,0.8)"  
-     document.querySelector(".prueba").style.transition= "0.8s"
+     document.querySelector(".prueba").style.transition= "5s"
+     document.querySelector(".logoCenter img").style.display= "none"
+     document.querySelector(".prueba").style.borderBottom= "1px white solid"
      
  }else {
      document.querySelector(".prueba").style.backgroundColor="transparent"
      document.querySelector(".prueba").style.borderBottom= "none"
+     document.querySelector(".logoCenter img").style.display= "inline"
+     document.querySelector(".logoCenter img").style.transition= "5s"
+   
  }
 })
