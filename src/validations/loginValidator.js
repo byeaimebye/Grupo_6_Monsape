51a6354ebb5d9d715446a78c63f8cfe5aec9b73c
AiméAPG <84512369+byeaimebye@users.noreply.github.com>
2021-10-26 15:52:29 -0300

    validaciones back de login ok, soy una crack

--- src/validations/loginValidator.js
+++ src/validations/loginValidator.js
@@ -4,26 +4,45 @@ const bcrypt = require('bcryptjs')
 const db = require('../database/models')
 
 
-let validations =[
-    body("email").custom((value,  {req}) => {
-        return db.User.findOne({ where: { email: value } })
-          .then((user) => {
-          if(!user){
-            return Promise.reject("El usuario no existe")
-          }
-          }),
-      body("password").custom((value, {req}) =>{
-        return db.User.findOne({ where : { email:value }})
-        .then((user) =>{ 
-        if (!user || !bcrypt.compareSync(req.body.password, user.password)){
-           Promise.reject("La contraseña que ingresaste no es correcta")
-         } })
-      })
-          .catch(() => {
-            return Promise.reject("Credenciales invalidas!");
-          });  
-      })
+module.exports = [
+  check('email')
+  .isEmail()
+  .withMessage('Debes ingresar un email válido'),
 
+  body('email').custom(value => {
+  
+    return db.User.findOne({
+        where : {
+            email : value
+        }
+    })
+    .then(user => {
+        if(!user){
+            return Promise.reject('Este email no está registrado')
+        }
+    })
+}),
+body('password').custom((value, {req}) => {
+return db.User.findOne({where: { email: req.body.email}})
+.then((user)=>{
+  if (!bcrypt.compareSync(value, user.password)){
+    return Promise.reject()
+  }
+}).catch(() => {
+  return Promise.reject("Las contraseñas no coinciden");
+});
+})
+  /* body("email").custom((value,  {req}) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (!user || !bcrypt.compareSync(req.body.password, user.password)){
+            return Promise.reject()
+          }
+        })
+        .catch(() => {
+          return Promise.reject("Credenciales invalidas!");
+        });
+    })
+ */
 ]
 
-module.exports = validations;
