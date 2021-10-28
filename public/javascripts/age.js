79d080a0a176dcf13cf442b2752d6c403def2dbc
AiméAPG <84512369+byeaimebye@users.noreply.github.com>
2021-10-28 13:16:07 -0300

    habia dejado la confirmacion de edad roto, sorry, hay que sacarlo de index y sus rutas esta sin terminar!

--- public/javascripts/age.js
+++ public/javascripts/age.js
@@ -2,6 +2,7 @@
 
 let age = document.querySelector(".home");
 age.onmouseover = function(){
-    document.querySelector(".main-ingreso").style.display = "block"
+    document.querySelector(".main-ingreso").style.display = "flex"
+    document.querySelector(".main-ingreso").style.position = "fixed"
     document.querySelector(".main-ingreso").style.transition = "10s"
 }
