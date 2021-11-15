2aa04e764cc3ff842b7c77a14fce71927f46fad3
FedeMontenegro <fedejulianmontenegro@gmail.com>
2021-11-15 11:25:30 -0300

    pre-checkout

--- public/javascripts/productCartDropDownScript.js
+++ public/javascripts/productCartDropDownScript.js
@@ -1,119 +1,138 @@
-const _cards = document.querySelector(".product-container");
-const _footer = document.querySelectorAll(".sub-sin-envio-drop-down p")
-const _templateCarrito = document.getElementById("template-carrito").content;
-const _templateFooter = document.getElementById("template-footer").content;
-const fragment = document.createDocumentFragment();
-let carrito = {};
+window.addEventListener("load", () => {
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    //fetchData();
-    if(localStorage.getItem("carrito")){
-        console.log("hola")
-        carrito = JSON.parse(localStorage.getItem("carrito"));
-        pintarCarrito()
+/* const _products2 = document.querySelector(".products");
+ */const _items = document.getElementById("items");
+const _verMas = document.getElementById("mas-drop-down");
+const _footer = document.querySelector(".subtotal-product");/* 
+const _templateCard = document.getElementById("template-card").content; */
+const _templateCarrito = document.getElementById("template-carrito").content;
+const _inputDropDown = document.querySelector(".cart-data-drop-down");
+const _labelCart = document.querySelector(".cart-data-label-drop-down img");
+const fragment2 = document.createDocumentFragment()
+
+//let url = `http://localhost:3080/api/products`;
+let carrito2 = {};
+
+document.addEventListener("DOMContentLoaded", e => {
+   /* fetchData(); */
+   if(localStorage.getItem("carrito")){
+    carrito2 = JSON.parse(localStorage.getItem("carrito"));
+    pintarCarrito()
+    }
+
+    if(window.location.href === "http://localhost:3080/products/tienda"){
+        _verMas.style.display = "none";
     }
 });
 
-_cards.addEventListener("click", e =>{
-    console.log("Funcionando")
+/* _products.addEventListener("click", e =>{
     addCarrito(e);
-});
+}) */
 
-/* items.addEventListener("click", e => {
+_items.addEventListener("click", e => {
     btnAccion(e);
-}) 
+    quitProduct(e);
+})
 
-const fetchData = async () => {
+//Realizando solicitud a la API...
+/* const fetchData = async () => {
     try {
-        const res = await fetch("productos.json")
-        const data = await res.json();
+        const res = await fetch(url)
+        const response = await res.json();
+        const data = await response.data;
         //console.log(data)
         pintarCards(data);
     } catch (error) {
         console.log(error);
     }
-}
+} */
 
-const pintarCards = data => {
+/* const pintarCards = data => {
     console.log(data)
     data.forEach(producto => {
-        templateCard.querySelector("h5").textContent = producto.title;
-        templateCard.querySelector("p").textContent = producto.precio;
-        templateCard.querySelector("img").setAttribute("src", producto.thumbnailUrl);
-        templateCard.querySelector(".btn-dark").dataset.id = producto.id;
+        _templateCard.querySelector("h4").textContent = producto.name;
+        _templateCard.querySelector(".precio").textContent = "$" + producto.price;
+        _templateCard.querySelector(".imagen-producto").setAttribute("src", "/img/"+producto.image);
+        _templateCard.querySelector(".agregar-producto-al-carrito").dataset.id = producto.id;
+        _templateCard.querySelector(".descuento-porcentaje p").textContent = Math.trunc(producto.discount) + "% OFF";
+        _templateCard.querySelector(".descuento").textContent = "$" + Math.round((100*producto.price)/(100-producto.discount));
 
-        const clone = templateCard.cloneNode(true);
-        fragment.appendChild(clone);
+        const clone = _templateCard.cloneNode(true);
+        fragment2.appendChild(clone);
     })
-    cards.appendChild(fragment);
-}*/
+    _products.appendChild(fragment2);
+} */
 
-const addCarrito = e => {
+/* const addCarrito = e => {
     console.log(e.target);
-    console.log(e.target.classList.contains("btn-buy"))
-    if(e.target.classList.contains("btn-buy")){
+    console.log(e.target.classList.contains("agregar-producto-al-carrito"))
+    if(e.target.classList.contains("agregar-producto-al-carrito")){
+        console.log("addCarrito")
         setCarrito(e.target.parentElement);//parentElement nos trae toda la información de la caja contenedora en la que se ejecuta un evento.
     }
     e.stopPropagation();
-}
+} */
 
-const setCarrito = objeto => {
+/* const setCarrito = objeto => {
     const producto = {
-        id: objeto.querySelector(".btn-buy").dataset.id,
-        title: objeto.getElementById("nombre").textContent,
-        precio: objeto.getElementById("precio").textContent,
+        id: objeto.querySelector(".agregar-producto-al-carrito").dataset.id,
+        title: objeto.querySelector("h4").textContent,
+        precio: objeto.querySelector(".precio").textContent,
+        imagen: objeto.querySelector(".imagen-producto").src,
         cantidad: 1
     }
-    console.log(producto)
 
-    if(carrito.hasOwnProperty(producto.id)){
-        producto.cantidad = carrito[producto.id].cantidad + 1;
+    if(carrito2.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito2[producto.id].cantidad + 1;
     }
 
-    carrito[producto.id] = {...producto};
+    carrito2[producto.id] = {...producto};
     pintarCarrito();
-}
+} */
 
 const pintarCarrito = () => {
-    console.log(carrito)
-    items.innerHTML = "";
-    Object.values(carrito).forEach(producto => {
-        _templateCarrito.querySelector("th").textContent = producto.id;
-        _templateCarrito.querySelectorAll("p")[0].textContent = producto.title;
-        _templateCarrito.querySelectorAll("p")[1].textContent = producto.cantidad * producto.precio;
+    _items.innerHTML = "";
+    Object.values(carrito2).forEach(producto => {
+        //_templateCarrito.querySelector("th").textContent = producto.id;
+        _templateCarrito.getElementById("nombre").textContent = producto.title;
+        _templateCarrito.getElementById("precio").textContent = "$"+(+producto.precio.substring(1)*(+producto.cantidad));
         _templateCarrito.querySelector(".mas").dataset.id = producto.id;
         _templateCarrito.querySelector(".menos").dataset.id = producto.id;
-        _templateCarrito.querySelector(".cantidad").textContent = producto.cantidad;
+        _templateCarrito.querySelector(".cantidad").value = producto.cantidad;
+        _templateCarrito.querySelector(".wine-image").src = producto.imagen;
+        _templateCarrito.querySelector(".tachito").dataset.id = producto.id;
 
         const clone = _templateCarrito.cloneNode(true);
-        fragment.appendChild(clone);
+        fragment2.appendChild(clone);
     })
-    items.appendChild(fragment);
+    _items.appendChild(fragment2);
 
     pintarFooter();
 
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito2));
 }
 
 const pintarFooter = () => {
-    _footer[1].innerHTML = "";
-    if(Object.keys(carrito).length === 0){
-        _footer[1].innerHTML = `
-            <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
-        `;
+    _footer.innerHTML = "$0";
+    if(Object.keys(carrito2).length === 0){
+        _items.innerHTML = `
+        <div class="container-not-found">
+            <h3 class="msg-cart-not-found">Tu carrito está vacío</h3>
+        </div>`
+        _footer.innerHTML = `$0`;
         return 
     }
-
-    //const nCantidad = Object.values(carrito).reduce((acc, {cantidad})=> acc + cantidad, 0);
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio})=> acc + cantidad * precio, 0)
     
-    //templateFooter.querySelectorAll("td")[0].textContent = nCantidad;
-    _templateFooter[1].textContent = nPrecio;
+    const nCantidad = Object.values(carrito2).reduce((acc, {cantidad})=> acc + cantidad, 0);
+    const nPrecio = Object.values(carrito2).reduce((acc, {cantidad, precio})=> acc + cantidad * +precio.substring(1), 0)
+    
+    _footer.textContent = "$"+nPrecio;
+    //_templateFooter.getElementById("precio").textContent = nPrecio;
 
-    const clone = _templateFooter.cloneNode(true);
-    fragment.appendChild(clone);
+    //const clone = _templateFooter.cloneNode(true);
+    //fragment2.appendChild(clone);
 
-    _footer.appendChild(fragment);
+    //_footer.appendChild(fragment2);
 
     /* const btnVaciar = document.getElementById("vaciar-carrito")
 
@@ -123,28 +142,41 @@ const pintarFooter = () => {
     }) */
 }
 
-//Botones de cantidad de producto.
 const btnAccion = (e) => {
     //Aumentar
     if(e.target.classList.contains("mas")){
-        console.log(e.target)
-        const producto = carrito[e.target.dataset.id];
-        producto.cantidad = carrito[e.target.dataset.id].cantidad + 1;
-        carrito[e.target.dataset.id] = {...producto}
-        console.log(carrito[e.target.dataset.id])
+        console.log("mas")
+        const producto = carrito2[e.target.dataset.id];
+        producto.cantidad = carrito2[e.target.dataset.id].cantidad + 1;
+        carrito2[e.target.dataset.id] = {...producto}
+        console.log(carrito2[e.target.dataset.id])
         pintarCarrito()
         pintarFooter()
+        _inputDropDown.checked = false;
     }
-
+    //Disminuir
     if(e.target.classList.contains("menos")){
-        console.log(e.target)
-        const producto = carrito[e.target.dataset.id];
-        producto.cantidad = carrito[e.target.dataset.id].cantidad - 1;
+        console.log("menos")
+        const producto = carrito2[e.target.dataset.id];
+        producto.cantidad = carrito2[e.target.dataset.id].cantidad - 1;
         if(producto.cantidad === 0){
-            delete carrito[e.target.dataset.id];
+            delete carrito2[e.target.dataset.id];
         }
         pintarCarrito()
         pintarFooter()
+        _inputDropDown.checked = false;
     }
     e.stopPropagation()
 }
+
+const quitProduct = e => {
+    if(e.target.className === "tachito"){
+        delete carrito2[e.target.dataset.id];
+    }
+    pintarCarrito()
+    pintarFooter()
+    _inputDropDown.checked = false;
+    e.stopPropagation()
+}
+
+});
--- public/javascripts/tiendaScript.js
+++ public/javascripts/tiendaScript.js
@@ -1,95 +1,179 @@
-window.addEventListener("load", (e)=>{
-    //e.preventDefault();
-    _products = document.querySelector(".products");
-    let url =`http://localhost:3080/api/products`;
-    let winesArray = [];
+const _productsTiendaScript = document.querySelector(".products");
+const _itemsTiendaScript = document.getElementById("items");
+const _verMasTiendaScript = document.getElementById("mas-drop-down");
+const _footer = document.querySelector(".subtotal-product");
+const _templateCard = document.getElementById("template-card").content;
+const _templateCarrito = document.getElementById("template-carrito").content;
+const _inputDropDown = document.querySelector(".cart-data-drop-down");
+const _labelCart = document.querySelector(".cart-data-label-drop-down img");
+const fragment = document.createDocumentFragment()
+console.log(_templateCard);
 
-     function wines(url){
-        fetch(url)
-            .then(response => { return response.json()})
-            .then(wines => {
-                
-                wines.data.forEach(wine => {
-                    winesArray.push(wine.name)
-                    _products.innerHTML += `
-                    <article class="product-container">
-                        <a href="/products/detail/${wine.id}"><img class="imagen-producto" src="/img/${wine.image} " alt="Imagen de vino Monsape"></a>                    
-                        <div class="descuento-porcentaje">
-                            <p> ${Math.trunc(wine.discount)}% OFF</p>
-                        </div>
-                        <div class="caja-de-vino">
-                            <img class="caja" src="/img/tienda/caja.svg" alt="caja de vino">
-                            <img class="cantidad-vinos" src="/img/tienda/6.svg" alt="cantidad de vinos en una caja">
-                        </div>
-                        <div class="descripcion-producto">
-                            <p class="precio-por-caja">Precio caja x6  botellas</p>
-                            <h4 class="nombre-producto">${wine.name}</h4>
-                        </div>
-                        <div class="descuento-y-precio">
-                            <span class="descuento">$ ${Math.round((100*wine.price)/(100-wine.discount))} </span>
-                            <span class="precio">$ ${wine.price}</span>
-                        </div>
-                        <button class="agregar-producto-al-carrito">Agregar</button>
-                    </article>`
-                })
-            })
-            .catch(err => {console.log(err)});
+let url = `http://localhost:3080/api/products`;
+let carrito = {};
 
+document.addEventListener("DOMContentLoaded", e => {
+   fetchData();
+   if(localStorage.getItem("carrito")){
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    pintarCarrito()
     }
 
-    function Alphabethicaly(array){
-        array.sort();
-        _products.innerHTML = ``;
-        array.forEach(wine => {
-            _products.innerHTML += `
-            <article class="product-container">
-                <a href="/products/detail/${wine.id}"><img class="imagen-producto" src="/img/${wine.image} " alt="Imagen de vino Monsape"></a>                    
-                <div class="descuento-porcentaje">
-                    <p> ${Math.trunc(wine.discount)}% OFF</p>
-                </div>
-                <div class="caja-de-vino">
-                    <img class="caja" src="/img/tienda/caja.svg" alt="caja de vino">
-                    <img class="cantidad-vinos" src="/img/tienda/6.svg" alt="cantidad de vinos en una caja">
-                </div>
-                <div class="descripcion-producto">
-                    <p class="precio-por-caja">Precio caja x6  botellas</p>
-                    <h4 class="nombre-producto">${wine.name}</h4>
-                </div>
-                <div class="descuento-y-precio">
-                    <span class="descuento">$ ${Math.round((100*wine.price)/(100-wine.discount))} </span>
-                    <span class="precio">$ ${wine.price}</span>
-                </div>
-                <button class="agregar-producto-al-carrito">Agregar</button>
-            </article>`
-        })
-
+    if(window.location.href === "http://localhost:3080/products/tienda"){
+        _verMasTiendaScript.style.display = "none";
     }
-    function AlphabethicalyReverse(array){
-        array.name.reverse();
-        _products.innerHTML = ``;
-        array.forEach(wine => {
-            _products.innerHTML += `
-            <article class="product-container">
-                <a href="/products/detail/${wine.id}"><img class="imagen-producto" src="/img/${wine.image} " alt="Imagen de vino Monsape"></a>                    
-                <div class="descuento-porcentaje">
-                    <p> ${Math.trunc(wine.discount)}% OFF</p>
-                </div>
-                <div class="caja-de-vino">
-                    <img class="caja" src="/img/tienda/caja.svg" alt="caja de vino">
-                    <img class="cantidad-vinos" src="/img/tienda/6.svg" alt="cantidad de vinos en una caja">
-                </div>
-                <div class="descripcion-producto">
-                    <p class="precio-por-caja">Precio caja x6  botellas</p>
-                    <h4 class="nombre-producto">${wine.name}</h4>
-                </div>
-                <div class="descuento-y-precio">
-                    <span class="descuento">$ ${Math.round((100*wine.price)/(100-wine.discount))} </span>
-                    <span class="precio">$ ${wine.price}</span>
-                </div>
-                <button class="agregar-producto-al-carrito">Agregar</button>
-            </article>`
-        })
-    }
+});
 
-    wines(url);
+_productsTiendaScript.addEventListener("click", e =>{
+    addCarrito(e);
 })
+
+_itemsTiendaScript.addEventListener("click", e => {
+    btnAccion(e);
+    quitProduct(e);
+})
+
+//Realizando solicitud a la API...
+const fetchData = async () => {
+    try {
+        const res = await fetch(url)
+        const response = await res.json();
+        const data = await response.data;
+        //console.log(data)
+        pintarCards(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const pintarCards = data => {
+    console.log(data)
+    data.forEach(producto => {
+        _templateCard.querySelector("h4").textContent = producto.name;
+        _templateCard.querySelector(".precio").textContent = "$" + producto.price;
+        _templateCard.querySelector(".imagen-producto").setAttribute("src", "/img/"+producto.image);
+        _templateCard.querySelector(".agregar-producto-al-carrito").dataset.id = producto.id;
+        _templateCard.querySelector(".descuento-porcentaje p").textContent = Math.trunc(producto.discount) + "% OFF";
+        _templateCard.querySelector(".descuento").textContent = "$" + Math.round((100*producto.price)/(100-producto.discount));
+
+        const clone = _templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    _productsTiendaScript.appendChild(fragment);
+}
+
+const addCarrito = e => {
+    console.log(e.target);
+    console.log(e.target.classList.contains("agregar-producto-al-carrito"))
+    if(e.target.classList.contains("agregar-producto-al-carrito")){
+        console.log("addCarrito")
+        setCarrito(e.target.parentElement);//parentElement nos trae toda la información de la caja contenedora en la que se ejecuta un evento.
+    }
+    e.stopPropagation();
+}
+
+const setCarrito = objeto => {
+    const producto = {
+        id: objeto.querySelector(".agregar-producto-al-carrito").dataset.id,
+        title: objeto.querySelector("h4").textContent,
+        precio: objeto.querySelector(".precio").textContent,
+        imagen: objeto.querySelector(".imagen-producto").src,
+        cantidad: 1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)){
+        producto.cantidad = carrito[producto.id].cantidad + 1;
+    }
+
+    carrito[producto.id] = {...producto};
+    pintarCarrito();
+}
+
+const pintarCarrito = () => {
+    _itemsTiendaScript.innerHTML = "";
+    Object.values(carrito).forEach(producto => {
+        //_templateCarrito.querySelector("th").textContent = producto.id;
+        _templateCarrito.getElementById("nombre").textContent = producto.title;
+        _templateCarrito.getElementById("precio").textContent = "$"+(+producto.precio.substring(1)*(+producto.cantidad));
+        _templateCarrito.querySelector(".mas").dataset.id = producto.id;
+        _templateCarrito.querySelector(".menos").dataset.id = producto.id;
+        _templateCarrito.querySelector(".cantidad").value = producto.cantidad;
+        _templateCarrito.querySelector(".wine-image").src = producto.imagen;
+        _templateCarrito.querySelector(".tachito").dataset.id = producto.id;
+
+        const clone = _templateCarrito.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    _itemsTiendaScript.appendChild(fragment);
+
+    pintarFooter();
+
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+const pintarFooter = () => {
+    _footer.innerHTML = "$0";
+    if(Object.keys(carrito).length === 0){
+        _itemsTiendaScript.innerHTML = `
+        <div class="container-not-found">
+            <h3 class="msg-cart-not-found">Tu carrito está vacío</h3>
+        </div>`
+        _footer.innerHTML = `$0`;
+        return 
+    }
+    
+    const nCantidad = Object.values(carrito).reduce((acc, {cantidad})=> acc + cantidad, 0);
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio})=> acc + cantidad * +precio.substring(1), 0)
+    
+    _footer.textContent = "$"+nPrecio;
+    //_templateFooter.getElementById("precio").textContent = nPrecio;
+
+    //const clone = _templateFooter.cloneNode(true);
+    //fragment.appendChild(clone);
+
+    //_footer.appendChild(fragment);
+
+    /* const btnVaciar = document.getElementById("vaciar-carrito")
+
+    btnVaciar.addEventListener("click", ()=> {
+        carrito = {};
+        pintarCarrito();
+    }) */
+}
+
+const btnAccion = (e) => {
+    //Aumentar
+    if(e.target.classList.contains("mas")){
+        console.log("mas")
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad = carrito[e.target.dataset.id].cantidad + 1;
+        carrito[e.target.dataset.id] = {...producto}
+        console.log(carrito[e.target.dataset.id])
+        pintarCarrito()
+        pintarFooter()
+        _inputDropDown.checked = false;
+    }
+    //Disminuir
+    if(e.target.classList.contains("menos")){
+        console.log("menos")
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad = carrito[e.target.dataset.id].cantidad - 1;
+        if(producto.cantidad === 0){
+            delete carrito[e.target.dataset.id];
+        }
+        pintarCarrito()
+        pintarFooter()
+        _inputDropDown.checked = false;
+    }
+    e.stopPropagation()
+}
+
+const quitProduct = e => {
+    if(e.target.className === "tachito"){
+        delete carrito[e.target.dataset.id];
+    }
+    pintarCarrito()
+    pintarFooter()
+    _inputDropDown.checked = false;
+    e.stopPropagation()
+}
