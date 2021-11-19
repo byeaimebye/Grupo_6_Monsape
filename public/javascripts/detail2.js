3d45e2819a6a21f725f00bf5edafa92632b05748
FedeMontenegro <fedejulianmontenegro@gmail.com>
2021-11-19 09:19:37 -0300

    Correcccciones a ventana modal de login

--- public/javascripts/detail2.js
+++ public/javascripts/detail2.js
@@ -1,15 +1,29 @@
 const modalAdd2 = document.querySelector('#modalAdd2');
+const ing = document.querySelector('#ing');
+const formdetail2 = document.querySelector('.form');
+const textFormdetail2 = document.querySelector('.text-forms');
+if(window.location.href != "http://localhost:3080/users/login"){
 
-const openModal2 = () => {
-    modalAdd2.style.display = 'flex';
-}
-
-const closeModal2 = () => {
-    modalAdd2.style.display = 'none';
-}
-
-modalAdd2 ? modalAdd2.onclick = (event) => {
-    if(event.target == modalAdd2){
-        closeModal2();
+    
+    ing.addEventListener("click", () => {
+        openModal2()
+    })
+    const openModal2 = () => {
+        modalAdd2.style.display = 'flex';
     }
-}:"";
+    
+    const closeModal2 = () => {
+        modalAdd2.style.display = 'none';
+    }
+    
+    modalAdd2 ? modalAdd2.onclick = (event) => {
+        if(event.target == modalAdd2){
+            closeModal2();
+        }
+    }:"";
+
+    formdetail2.style.textAlign = "center";
+    textFormdetail2.style.marginLeft = "";
+} else {
+    modalAdd2.innerHTML = "";
+}
